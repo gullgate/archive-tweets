afe0a43b85b13eaea266445751c2d90d45fa6674
Bjarni R. Einarsson <bre@klaki.net>
2012-10-16 19:02:00 +0000

    Customize for myself, capture conversations as well, tweaked output format to be more scripting friendly

--- archive-tweets.py
+++ archive-tweets.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python
 
-import tweepy
-import pytz
 import os
+import pytz
+import sys
+import tweepy
+
+# Personalization
+me = len(sys.argv) > 1 and sys.argv[1] or 'HerraBRE'
+tweetdir = os.environ['HOME'] + '/bre.pagekite.me/twitter/'
+homeTZ = pytz.timezone('GMT+0')
 
 # Parameters.
-me = 'username'
-urlprefix = 'http://twitter.com/%s/status/' % me
-tweetdir = os.environ['HOME'] + '/Dropbox/twitter/'
-tweetfile = tweetdir + 'twitter.txt'
-idfile = tweetdir + 'lastID.txt'
-datefmt = '%B %-d, %Y at %-I:%M %p'
-homeTZ = pytz.timezone('US/Central')
-utc = pytz.utc
+maxtweets = 1000
+urlprefix = 'http://twitter.com/%s/status/'
+tweetfile = tweetdir + me + '.txt'
+idfile = tweetdir + me + '-lid.txt'
+datefmt = '%B %-d, %Y at %-I:%M %p (%s)'
 
 def setup_api():
   """Authorize the use of the Twitter API."""
@@ -29,24 +32,51 @@ def setup_api():
 api = setup_api()
 
 # Get the ID of the last downloaded tweet.
-with open(idfile, 'r') as f:
-  lastID = f.read().rstrip()
+lastID = '204960244940537858'
+try:
+  with open(idfile, 'r') as f:
+    lastID = f.read().rstrip()
+except IOError:
+  pass
 
 # Collect all the tweets since the last one.
-tweets = api.user_timeline(me, since_id=lastID, count=200, include_rts=True)
+tweets = []
+tweets.extend(api.user_timeline(me, since_id=lastID,
+                                    count=maxtweets,
+                                    include_rts=True))
+tweets.extend(api.favorites(me))
+
+# Collect all recent mentions of this user, via. the search API.
+if '--search' in sys.argv:
+  tweets.extend(api.search('%s' % me, count=maxtweets))
+else:
+  tweets.extend(api.search('@%s' % me, count=maxtweets))
+
+# Sort them by ID...
+tweets.sort(key=lambda t: t.created_at)
 
 # Write them out to the twitter.txt file.
+maxID = lastID
+utc = pytz.utc
 with open(tweetfile, 'a') as f:
-    for t in reversed(tweets):
+  for t in tweets:
+    if t.id_str > lastID and t.id_str != maxID:
       ts = utc.localize(t.created_at).astimezone(homeTZ)
-      lines = ['',
-               t.text,
-               ts.strftime(datefmt).decode('utf8'),
-               urlprefix + t.id_str,
-               '- - - - -',
-               '']
+      try:
+        frm = t.from_user
+      except AttributeError:
+        frm = me
+      # TODO: Clean up t.co URLs by using this trick:
+      #       curl -sLI http://t.co/Hy13FmLM |grep Location:
+      lines = ['From: @%s' % frm,
+               'Text: %s' % t.text,
+               'Date: %s' % ts.strftime(datefmt).decode('utf8'),
+               'Link: %s%s' % (urlprefix % frm, t.id_str),
+               '', '']
       f.write('\n'.join(lines).encode('utf8'))
-      lastID = t.id_str
+      if t.id_str > maxID:
+        maxID = t.id_str
+lastID = maxID
 
 # Update the ID of the last downloaded tweet.
 with open(idfile, 'w') as f:
